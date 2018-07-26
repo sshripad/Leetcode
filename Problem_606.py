3af0f14c474524002389509fe62c2f5dfc84da59
Shriharsha Shripadaraj <shriharshashripadaraj@Shriharshas-MacBook-Air.local>
2018-07-26 11:54:13 -0700

    Day_26

---
+++ Problem_606.py
@@ -0,0 +1,44 @@
+'''
+You need to construct a string consists of parenthesis and integers from a binary tree with the preorder traversing way.
+
+The null node needs to be represented by empty parenthesis pair "()". And you need to omit all the empty parenthesis pairs that don't affect the one-to-one mapping relationship between the string and the original binary tree.
+
+Example 1:
+Input: Binary tree: [1,2,3,4]
+       1
+     /   \
+    2     3
+   /    
+  4     
+
+Output: "1(2(4))(3)"
+
+Explanation: Originallay it needs to be "1(2(4)())(3()())", 
+but you need to omit all the unnecessary empty parenthesis pairs. 
+And it will be "1(2(4))(3)".
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def tree2str(self, t):
+        """
+        :type t: TreeNode
+        :rtype: str
+        """
+        return(self.getans(t))
+            
+    def getans(self,t):
+        if not t:
+            return ''
+        if not t.left and not t.right:
+            return str(t.val)
+        if t.left and not t.right:
+            return str(t.val)+'('+self.getans(t.left)+')'
+        return str(t.val)+'('+self.getans(t.left)+')'+'('+self.getans(t.right)+')'
+        
